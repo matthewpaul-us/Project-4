b0a2ea4327613f308a5c1de8e547f56c965baa5e
Matthew Paul <mpaul0416@gmail.com>
2012-04-15 19:42:08 -0400

    First commit

---
+++ TypingTutor/src/core/Word.java
@@ -0,0 +1,28 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Word.java<br/>
+ * Project name: TypingTutor<br/>
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Matthew Paul, paulmr@goldmail.etsu.edu<br/>
+ * Course:  CSCI 1260-088<br/>
+ * Creation Date: Apr 15, 2012<br/>
+ * Date of Last Modification: Apr 15, 2012
+ * ---------------------------------------------------------------------------
+ */
+
+package core;
+
+
+/**
+ * Represents a word for a typing tutor game<br>
+ *
+ * <hr>
+ * Date created: Apr 15, 2012<br>
+ * Date last modified: Apr 15, 2012<br>
+ * <hr>
+ * @author Matthew Paul
+ */
+public class Word
+{
+	
+}
